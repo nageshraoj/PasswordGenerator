ca328af71be40a27fb77c2f87243662c5196b9fd
nageshraoj <nageshraoj@gmail.com>
2020-08-29 15:27:38 +0530

    Password Generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app2';
+  password:any;
+  PwdLength:number;
+  isNumbers=false;
+  isCaptitals=false;
+  isLowers=false;
+  isSymbols=false;
+
+  UpdateNumbers(){
+    this.isNumbers = !this.isNumbers;
+  }
+  UpdateCapitals(){
+    this.isCaptitals=!this.isCaptitals;
+  }
+
+ UpdateLowers(){
+    this.isLowers=!this.isLowers;
+  }
+  
+  UpdateSymbols(){
+    this.isSymbols=!this.isSymbols;
+  }
+  
+  updatepassword(pwdChar){
+    this.PwdLength= parseInt( pwdChar.target.value);
+  }
+
+  genartePassword(){
+    this.password='';
+    let PwdChars='';
+    PwdChars = this.isNumbers?"01234567890":"";
+    PwdChars = this.isCaptitals?PwdChars+"ABCDEFGHIJKLMNOPQRSTUVWXYZ":PwdChars;
+    PwdChars = this.isLowers?PwdChars+"abcdefghijklmnopqrstuvwxyz":PwdChars;
+    PwdChars = this.isSymbols?PwdChars+"!@#$%^&*()":PwdChars;
+
+    for (let index = 0; index < this.PwdLength; index++) {
+      const rnd = this.randomNumber(PwdChars.length);
+      this.password+=PwdChars.substring(rnd,rnd+1);
+    }
+
+  }
+
+  randomNumber(max:any){
+    return Math.floor(Math.random() * max);
+  }
+
 }
